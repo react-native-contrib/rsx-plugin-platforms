bb028818ed4e3e98ec5edba453669e0247fac283
Hassan Khan <contact@hassankhan.me>
2016-05-08 20:06:48 +0100

    Fix issue with incorrect current working directory

--- src/add.js
+++ src/add.js
@@ -1,10 +1,10 @@
-const utils = require('rsx-common');
-const path  = require('path');
-const env   = require('yeoman-environment').createEnv();
+const utils  = require('rsx-common');
+const path   = require('path');
+const yeoman = require('yeoman-environment');
 
 const log   = utils.log;
 
-const registerGenerators = () => {
+const registerGenerators = (env) => {
     env.register(require.resolve('rsx-generator-ios'), 'rsx:ios');
     env.register(require.resolve('rsx-generator-android'), 'rsx:android');
 };
@@ -12,13 +12,15 @@ const registerGenerators = () => {
 module.exports = function add(args, callback) {
     log.heading    = 'rsx-platforms add';
     const platform = args;
+    const rootPath = process.cwd();
     const appRoot  = process.env.RN_PROJECT_ROOT;
     const name     = utils.path.getProjectFolderName(appRoot);
+    process.chdir(appRoot);
 
-    registerGenerators();
-
+    const env = registerGenerators(yeoman.createEnv());
     env.run([`rsx:${platform}`, name], () => {
         log.info(`The ${platform} platform has been added to ${appRoot}`);
+        process.chdir(rootPath);
 
         if (callback) { callback(); }
     });
