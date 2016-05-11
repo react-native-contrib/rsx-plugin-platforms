ebd516dc83f9fc220293d806e5ec48e2f9deb616
Hassan Khan <contact@hassankhan.me>
2016-05-11 03:01:16 +0100

    Added project files

---
+++ tests/platforms.spec.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const mock = require('mock-require');
+const path = require('path');
+
+const expect = chai.expect;
+
+const command = require('../src/platforms');
+
+describe('platforms', () => {
+
+    it('should do something');
+
+});
