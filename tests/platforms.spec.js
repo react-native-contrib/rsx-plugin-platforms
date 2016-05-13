3f790032c70949f08bf5a9c282252bb0a2c38350
Hassan Khan <contact@hassankhan.me>
2016-05-13 17:37:33 +0100

    Added more tests

--- tests/platforms.spec.js
+++ tests/platforms.spec.js
@@ -1,14 +1,66 @@
+const utils = require('rsx-common');
 const chai = require('chai');
-const sinon = require('sinon');
 const mock = require('mock-require');
+const sinon = require('sinon');
 const path = require('path');
 
 const expect = chai.expect;
+const log = utils.log;
 
-const command = require('../src/platforms');
+log.level = 'silent';
 
 describe('platforms', () => {
 
-    it('should do something');
+    it('should throw an error if an invalid action is specified', () => {
+        process.env.RN_PROJECT_ROOT = path.join(__dirname, 'fixtures');
+        mock('rsx-common', {
+            log: log,
+            process: {
+                run: () => { return (callback) => { callback(arguments); }; },
+            },
+            path: utils.path,
+            project: utils.project,
+            validate: utils.validate,
+        });
+        const spy = sinon.spy();
+        const command = require('../src/platforms');
+        command(['pppppp'], spy);
+        expect(spy.calledWith('pppppp is not a valid action for this command'));
+        mock.stop('rsx-common');
+    });
+
+    it('should execute the subcommand if a valid action is specified', () => {
+        const spy = sinon.spy();
+        const command = require('../src/platforms');
+        command(['ls'], spy);
+
+        expect(spy.calledOnce).to.deep.equals(true);
+    });
+
+    describe('ls', () => {
+
+        it('should show a list of installed platforms', () => {
+            process.env.RN_PROJECT_ROOT = path.join(__dirname, 'fixtures');
+            var result;
+            const command = require('../src/list');
+            command({}, (platforms) => {
+                result = platforms;
+            });
+
+            expect(result).to.deep.equals(['android', 'ios']);
+        });
+
+    });
+
+    describe('add', () => {
+
+        it('should add a platform project for React Native');
+    });
+
+    describe('rm', () => {
+
+        it('should remove a platform project for React Native');
+
+    });
 
 });
