8d044a242fe35bcbf54e336bf071f9f95497608d
Hassan Khan <contact@hassankhan.me>
2016-05-13 17:37:25 +0100

    Cleanup

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = [{
-  func: require('./src/platforms'),
-  description: 'Interact with target platforms',
-  name: 'platforms <action> [platformName]',
+    func: require('./src/platforms'),
+    description: 'Interact with target platforms',
+    name: 'platforms <action> [platformName]',
 }];
